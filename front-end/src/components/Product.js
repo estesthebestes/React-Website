21147139f454a7c95e6eaaa95cec8e41ee9475de
Hunter <esteshunter22@gmail.com>
2023-02-13 15:49:33 -0600

    fixed error with my closing parathesis

--- front-end/src/components/Product.js
+++ front-end/src/components/Product.js
@@ -4,25 +4,27 @@ import { Card } from 'react-bootstrap';
 const Product = ({ product }) => {
 	return (
 		<Card className='my-3 p-3 rounded'>
-			<a href={`/product/${product._id}`} />
-			<Card.Img src={product.image} variant='top' />
+			<a href={`/product/${product._id}`}>
+				<Card.Img src={product.image} variant='top' />
+			</a>
+
+			<Card.Body>
+				<a href={`/product/${product._id}`}>
+					<Card.Title as='div'>
+						<strong>{product.name}</strong>
+					</Card.Title>
+				</a>
+
+				<Card.Text as='div'>
+					<div className='my-3'>
+						{product.rating} from {product.numReviews} reviews
+					</div>
+				</Card.Text>
+
+				<Card.Text as='h3'>${product.price}</Card.Text>
+			</Card.Body>
 		</Card>
 	);
 };
-
-<Card.Body>
-	<a href={`/product/${product._id}`}>
-		<Card.Title as='div'>
-			<strong>{Product.name}</strong>
-		</Card.Title>
-	</a>
-	<Card.Text as='div'>
-		<div className='my-3'>
-			{Product.rating} from {Product.numReviews} reviews
-		</div>
-	</Card.Text>
-
-	<Card.Text as='h3'> ${Product.price}</Card.Text>
-</Card.Body>;
 
 export default Product;
